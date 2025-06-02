be70b740525dbd3c2425a87f69a58f167472ebb8
RK <rk@whitehatglobal.org>
2025-06-02 10:47:44 +0530

    Feat: Capture the Contact and document name in ticket

--- quickhelp/utils/support.py
+++ quickhelp/utils/support.py
@@ -2,13 +2,14 @@
 # For license information, please see license.txt
 
 import frappe
+import json
 from frappe.utils import cint, flt, get_url, now
 from frappe.utils.safe_exec import get_safe_globals, safe_eval
 from quickhelp.utils.requests import make_request
 from typing import Optional
 
 @frappe.whitelist()
-def create_ticket(title: str, raised_by: str, ticket_module: str, description: str) -> str:
+def create_ticket(title: str, raised_by: str, ticket_module: str, description: str, kly_contact_no: str, kly_reference_doc: str ) -> str:
     settings = frappe.get_cached_doc("QuickHelp Settings")
     headers = {
         "Authorization": f"token {settings.get_password('support_api_token')}",
@@ -21,10 +22,11 @@ def create_ticket(title: str, raised_by: str, ticket_module: str, description: s
             "subject": title,
             "kly_module": ticket_module,
             "customer":settings.project_id,
+            "kly_contact_no": kly_contact_no,
+            "kly_reference_doc": kly_reference_doc,
             # **generate_ticket_details(settings),
         },
     }
-
     response = make_request(
         url=f"{settings.support_url}/api/method/helpdesk.helpdesk.doctype.hd_ticket.api.new",
         headers=headers,
